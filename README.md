25ae4d06600b31ac907b6cd9138d360a95a9d601
dimich² <164957381+samboryk@users.noreply.github.com>
2025-12-19 22:01:46 +0200

    Create simple scary maze level

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # maze
+
+A tiny "scary maze" made with Godot. Guide the black dot to the red goal without touching the black walls. If you collide, a jumpscare screen flashes before the game closes. Press **R** any time to restart the level.
--- dot.gd
+++ dot.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-func _process(delta):
-	global_position = get_viewport().get_mouse_position()
+func _process(_delta):
+    global_position = get_viewport().get_mouse_position()
 
 func _on_area_entered(area):
-	if area.is_in_group("wall"):
-		get_tree().change_scene_to_file("res://jumpscare.tscn")
-	elif area.is_in_group("finish"):
-		get_tree().change_scene_to_file("res://jumpscare.tscn")
+    if area.is_in_group("wall"):
+        get_tree().change_scene_to_file("res://jumpscare.tscn")
+    elif area.is_in_group("finish"):
+        get_tree().change_scene_to_file("res://jumpscare.tscn")
